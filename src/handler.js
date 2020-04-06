fd155e824d235227b108c6c7b83760a6de9696c3
Michael Kazmier <dakazmier@gmail.com>
2020-04-06 08:53:28 -0600

    refactor users to controller

--- src/handler.js
+++ src/handler.js
@@ -2,61 +2,9 @@ const AWS = require('aws-sdk')
 AWS.config.update({region: 'us-east-1'});
 const DocumentClient = new AWS.DynamoDB.DocumentClient()
 const app = require('lambda-api')({ version: 'v1.0', base: 'v1/' })
-const { User } = require('./models/User')
+const usersController = require('./usersController')
 
-
-/**
- *  Get All Users
- */
-app.get('/users', async (req,res) => {
-  try {
-    const queryParams = User.query('user', { index: 'gsi1', limit: 10 })
-    const response = await DocumentClient.query(queryParams).promise()
-    const users = User.parse(response.Items)
-    res.status(200).json({ status: 'ok', users })
-  } catch (err) {
-    console.error('Error fetching users', err)
-    res.sendStatus(500)
-  }
-})
-
-/**
- *  Create a User
- */
-app.post('/users', async (req,res) => {
-  try {
-    const putParams = User.put(req.body)
-    const response = await DocumentClient.put(putParams).promise()
-    const user = User.parse(response.Item)
-    res.status(200).json({ status: 'ok', user })
-  } catch (err) {
-    console.error('Error creating user', req.body, err)
-    res.sendStatus(500)
-  }
-})
-
-/**
- *  Get One Users
- */
-app.get('/users/:userId', async (req,res) => {
-  try {
-    const getParams = User.get({pk: `user#${req.params.userId }`})
-    console.log({getParams})
-    const response = await DocumentClient.get(getParams).promise()
-    const user = User.parse(response.Item)
-    res.status(200).json({ status: 'ok', user })
-  } catch (err) {
-    console.error('Error fetching user', req.params.userId, err)
-    res.sendStatus(500)
-  }
-})
-
-app.patch('/users/:userId', (req,res) => {
-  res.status(200).json({ status: 'ok', userId: req.params.userId })
-})
-
-
-
+usersController(app, DocumentClient)
 
 module.exports.router = (event, context, callback) => {
 
---
+++ src/usersController.js
@@ -0,0 +1,53 @@
+const { User } = require('./models/User')
+
+module.exports = function(app, DocumentClient) {
+    /**
+   *  Get All Users
+   */
+  app.get('/users', async (req,res) => {
+    try {
+      const queryParams = User.query('user', { index: 'gsi1', limit: 10 })
+      const response = await DocumentClient.query(queryParams).promise()
+      const users = User.parse(response.Items)
+      res.status(200).json({ status: 'ok', users })
+    } catch (err) {
+      console.error('Error fetching users', err)
+      res.sendStatus(500)
+    }
+  })
+
+  /**
+   *  Create a User
+   */
+  app.post('/users', async (req,res) => {
+    try {
+      const putParams = User.put(req.body, { ReturnValues: 'ALL_NEW' })
+      const response = await DocumentClient.put(putParams).promise()
+      const user = User.parse(response.Item)
+      res.status(200).json({ status: 'ok', user })
+    } catch (err) {
+      console.error('Error creating user', req.body, err)
+      res.sendStatus(500)
+    }
+  })
+
+  /**
+   *  Get One User
+   */
+  app.get('/users/:userId', async (req,res) => {
+    try {
+      const getParams = User.get({pk: `user#${req.params.userId }`})
+      console.log({getParams})
+      const response = await DocumentClient.get(getParams).promise()
+      const user = User.parse(response.Item)
+      res.status(200).json({ status: 'ok', user })
+    } catch (err) {
+      console.error('Error fetching user', req.params.userId, err)
+      res.sendStatus(500)
+    }
+  })
+
+  app.patch('/users/:userId', (req,res) => {
+    res.status(200).json({ status: 'ok', userId: req.params.userId })
+  })
+}
